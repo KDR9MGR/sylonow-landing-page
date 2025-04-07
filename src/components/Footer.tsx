ee29f64fef5c99e93d7ae3e88ba01de433a5bf14
Technogeekpro <arbazkdr7866@gmail.com>
2025-04-07 17:53:58 +0530

    minor changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,27 +1,23 @@
-import React from 'react';
-import { Linkedin, Mail, Instagram, Facebook, Gift } from 'lucide-react';
-import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import { Linkedin, Mail, Instagram, Facebook, Gift } from "lucide-react";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
-  
+
   const socialLinks = [
     {
-      name: 'LinkedIn',
-      href: 'https://www.linkedin.com/in/sylonow?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
-      icon: <Linkedin className="h-6 w-6" />
+      name: "LinkedIn",
+      href: " https://www.linkedin.com/company/sylonow-main/",
     },
     {
-      name: 'Instagram',
-      href: 'https://www.instagram.com/sylonow',
-      icon: <Instagram className="h-6 w-6" />
+      name: "Instagram",
+      href: "https://www.instagram.com/sylonow",
+      icon: <Instagram className="h-6 w-6" />,
     },
     {
-      name: 'Facebook',
-      href: 'https://www.facebook.com/share/153oaG1MHN/',
-      icon: <Facebook className="h-6 w-6" />
-    }
+      name: "Facebook",
+      href: "https://www.facebook.com/share/153oaG1MHN/",
+      icon: <Facebook className="h-6 w-6" />,
+    },
   ];
 
   return (
@@ -29,8 +25,8 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col items-center space-y-8">
           {/* Logo */}
-          <img src="/lovable-uploads/logo.png" alt="Sylonow Logo" className="h-12" />
-          
+          <img src="/web-logo.png" alt="Sylonow Logo" className="h-12" />
+
           {/* Social Links */}
           <div className="flex space-x-6">
             {socialLinks.map((item) => (
